0f4067f62447b3b0dd46cfb51716a5023059dd11
Javier Moreno <jmorenocarrero@hotmail.com>
2023-04-25 11:39:02 +0200

    First commit

--- app/src/main/java/com/jmoreno/dragonballandroid/MainActivity.kt
+++ app/src/main/java/com/jmoreno/dragonballandroid/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.jmoreno.dragonballandroid
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val nombreUsuario = "Javier"
+    private val password = "230681"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadFromPreferences()
     }
+    private fun loadFromPreferences() {
+        //val shared = getPreferences(Context.MODE_PRIVATE)
+        //val numOld = shared.getInt("MiInteger", 0)
+
+        //Revisar: let, apply, with, run
+        getPreferences(Context.MODE_PRIVATE).apply {
+            Log.w("tag", "${getString("MiNombre", "")}")
+            Log.w("tag", "${getString("MiPassword", "")}")
+        }
+
+    }
+
+    override fun onStop() {
+        saveFromPreferences()
+        super.onStop()
+
+    }
+    private fun saveFromPreferences() {
+        // val shared = getPreferences(Context.MODE_PRIVATE)
+        // val sharedPreferencesEditable = shared.edit()
+        // sharedPreferencesEditable.putInt("MiInteger",numRandom)
+        // sharedPreferencesEditable.apply()
+
+        getPreferences(Context.MODE_PRIVATE).edit().apply {
+            putString("MiNombre",nombreUsuario)
+            putString("MiPassword", password)
+            apply()
+        }
+
+    }
+
 }
